0330aea3b1a1f6a3950bb7ab8e8914780eb9a42e
Andres Hernandez <zariel145@gmail.com>
2026-02-17 13:59:19 -0500

    chore: add helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Layout } from './components/Layout/Layout';
+import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 
 export default function App() {
@@ -6,6 +7,16 @@ export default function App() {
 
   return (
     <Layout>
+      <Helmet
+        htmlAttributes={{ lang: i18n.language }}
+        title={`${t('title')} | v2`}
+        meta={[
+          {
+            name: 'description',
+            content: t('subtitle'),
+          },
+        ]}
+      />
       <h1>{t('title')}</h1>
       <p>{t('subtitle')}</p>
       <p>
--- src/i18n/locales/es.ts
+++ src/i18n/locales/es.ts
@@ -1,8 +1,9 @@
 export const es = {
-  title: 'Hola',
-  subtitle: 'Tu proyecto ahora tiene internacionalización configurada.',
-  languageLabel: 'Idioma',
-  currentLanguage: 'Actual: {{lng}}',
-  switchToEnglish: 'Inglés',
-  switchToSpanish: 'Español',
+  title: 'Andrés Hernández Frontend Engineer',
+  subtitle:
+    'Andrés is a front-end developer who focuses on building scalable, accessible, and user-centered web applications. He transforms ideas into high-performance digital experiences through modern technologies and clean, intentional design.',
+  language_label: 'Idioma',
+  language_current: 'Actual: {{lng}}',
+  language_switch_to_english: 'Inglés',
+  language_switch_to_spanish: 'Español',
 } as const;
